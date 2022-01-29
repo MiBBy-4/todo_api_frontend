700f7de534dd5dc809b87da8a1f7dd793e603806
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-29 14:39:26 +0300

    feat: add getTasks method inside TodoList class for fetch API and get Todo tasks like an item, also create TodoItem class to show all items from API

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react"
 import TodoForm from './TodoForm'
+import TodoItem from './TodoItem'
 
-const api_url = 'http://localhost:300/api/v1/todos'
+const api_url = 'http://localhost:3000/api/v1/todos'
 
 class TodoList extends Component
 {
@@ -12,19 +13,31 @@ class TodoList extends Component
       items: []
     }
   }
+  componentDidMount()
+  {
+    this.getTasks()
+  }
+
+  getTasks()
+  {
+    fetch(api_url)
+    .then(response => response.json())
+    .then(response_items => {
+      this.setState({
+        items: response_items
+      })
+    })
+  }
 
   render()
   {
     return(
       <div>
         <TodoForm />
-        <ul>
-          <li>
-            Todo #1
-          </li>
-          <li>
-            Todo #2
-          </li>
+        <ul id='todo_list'>
+         {this.state.items.map((item) => (
+           <TodoItem key={item.id} item={item}/>
+         ))}
         </ul>
       </div>
     )
