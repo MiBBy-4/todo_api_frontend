ea62faf9da448f2ba80eb4686335a977ea9a8de0
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-30 14:14:12 +0300

    refactor: some changes in front side with styles and tags

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList/TodoList'
-import { Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles'
+import { Typography } from "@mui/material"
+
+const useStyles = makeStyles({
+  app_title: {
+    textAlign: 'center',
+  }
+})
+
 function App() {
+  const classes = useStyles()
   return (
     <div className="App">
       <header className="App-header">
-        <Typography variant="h1">Todo Stream</Typography>
+        <Typography variant="h1" className={classes.app_title}>Todo Stream</Typography>
         <hr />
         <TodoList />
       </header>
--- src/TodoList/TodoItem.js
+++ src/TodoList/TodoItem.js
@@ -1,14 +1,31 @@
 import React, {Component} from "react"
-import { Grid } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import Paper from "@mui/material/Paper"
 import { makeStyles } from '@mui/styles'
 import { Button } from "@mui/material"
 import DeleteIcon from '@mui/icons-material/Delete';
+import { textAlign } from "@mui/system"
 
 const useStyles = makeStyles({
   root: {
     height: 'auto',
     padding: '2em',
+    margin: '1em'
+  },
+  paper: {
+    margin: '1em',
+    padding: '2em',
+    textAlign: 'justify'
+  },
+  heading: {
+    textAlign: 'center',
+
+  },
+  todo_body:{
+    padding: '1em',
+  },
+  divider:{
+    width: '25%',
     margin: '1em'
   }
 })
@@ -20,13 +37,20 @@ export default function TodoItem(props) {
     props.deleteItem(props.item)
   }
   return(
-    <Grid container spacing={0}> 
-      <Grid item xs={9}>
-        <Paper elevation={3} className = {classes.root}>
-          {props.item.task}
-          <hr />
-          {props.item.body}
-          <hr />
+    <Grid container spacing={0}>
+      <Grid item xs={1}></Grid> 
+      <Grid item xs={10}>
+        <Paper elevation={10} className = {classes.paper}>
+          <Typography variant='h4' className={classes.heading}>
+            {props.item.task}
+          </Typography>
+          <hr className={classes.divider} style={{ marginLeft: '37.5%' }}/>
+          <div className={classes.todo_body} >
+            <Typography variant='body1'>
+              {props.item.body}
+            </Typography>
+          </div>
+          <hr className={classes.divider} style={{ marginLeft: '37.5%' }}/>
           <Button variant="contained" color="secondary" size="small" startIcon={<DeleteIcon />} onClick={handleDelete}> Delete task </Button>
         </Paper>
       </Grid>
