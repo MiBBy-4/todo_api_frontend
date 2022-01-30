d2bb1a40b66bc13d1efaf7433d28bf0b92bcab4e
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-30 14:30:55 +0300

    feat: create handle for Body field and add opportunity to clear all fields in the form after submit

--- src/TodoList/TodoForm.js
+++ src/TodoList/TodoForm.js
@@ -9,13 +9,19 @@ class TodoForm extends Component
   constructor(props)
   {
     super(props)
+    let defaultTask = ""
+    let defaultBody = ""
     this.state = {
       api_url: props.api_url,
-      task: "",
+      task: '',
+      body: '',
+      defaultTaskValue: defaultTask,
+      defaultBodyValue: defaultBody,
 
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleTaskChange = this.handleTaskChange.bind(this)
+    this.handleBodyChange = this.handleBodyChange.bind(this)
   }
 
   handleSubmit(event)
@@ -33,12 +39,23 @@ class TodoForm extends Component
       body: data
     }).then(response => response.json())
     .then(response => this.props.updateTodoList(response))
+    this.setState({
+      task: this.state.defaultTaskValue,
+      body: this.state.defaultBodyValue
+    })
   }
 
   handleTaskChange(event)
   {
     this.setState({
       task: event.target.value
+    })
+  }
+
+  handleBodyChange(event)
+  {
+    this.setState({
+      body: event.target.value
     })
   }
 
@@ -51,10 +68,10 @@ class TodoForm extends Component
           <form onSubmit={this.handleSubmit} id='todo_form' autoComplete='off'> 
             <Grid container>
               <Grid item xs={12}>
-                <TextField id='task_input' label='Your task' variant='outlined' type='text' name='todo[task]' onChange={this.handleTaskChange} fullWidth/>
+                <TextField id='task_input' label='Your task' variant='outlined' type='text' name='todo[task]' onChange={this.handleTaskChange} fullWidth value={this.state.task}/>
               </Grid>
               <Grid item xs={12}>
-                <TextareaAutosize id='body_input' label='Task body' variant='outlined' type='text' name='todo[body]' style={{ width: '99.5%', marginTop: '5px' }} minRows={3} placeholder='Describe your todo item.'/>
+                <TextareaAutosize id='body_input' label='Task body' variant='outlined' type='text' name='todo[body]' style={{ width: '99.5%', marginTop: '5px' }} minRows={3} placeholder='Describe your todo item.' value={this.state.body} onChange={this.handleBodyChange}/>
               </Grid>
               <Grid item xs={2}>
                 <Button variant='contained' color='primary' type='submit' style={{height: "100%"}}> Add task </Button>
